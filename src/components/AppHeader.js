44df27a001b2d125394f821bd2ee267533f4374f
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-07-25 22:57:50 +0300

    fix: исправить название

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -4,7 +4,7 @@ import './../blocks/app-header/app-header.css';
 const AppHeader = () => {
   return (
     <div className='app-header'>
-      <h1 className='app-header__title'>My Todo list</h1>
+      <h1 className='app-header__title'>Todo List</h1>
       <span className='app-header__info'>1 more to do, 3 done</span>
     </div>
   );
