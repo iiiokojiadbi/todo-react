ce50a4bafd76b894a4219b626db8b883d2328f41
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-07-25 22:28:18 +0300

    feat: доработать компонент AppHeader

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React from 'react';
+import './../blocks/app-header/app-header.css';
 
 const AppHeader = () => {
-  return <h1>My Todo list</h1>
+  return (
+    <div className='app-header'>
+      <h1 className='app-header__title'>My Todo list</h1>
+      <span className='app-header__info'>1 more to do, 3 done</span>
+    </div>
+  )
 }
 
 export default AppHeader;
