15d28978a9a4c2efa95f26ce34454ed1754bef63
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-07-25 21:35:19 +0300

    feat: добавить группы кнопок

--- src/components/SearchPanel.js
+++ src/components/SearchPanel.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import SearchInput from './SearchInput'
+import Button from './Button';
 
 const SearchPanel = () => {
   
   return (
-    <SearchInput />
+    <React.Fragment>
+      <SearchInput />
+      <div className="btn-group" role="group">
+        <Button text={ 'All' } />
+        <Button text={ 'Active' } />
+        <Button text={ 'Done' } />
+      </div>
+    </React.Fragment>
   )
 }
 
