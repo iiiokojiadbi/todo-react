027bb99f972b06cba1a82d2d025c8f3f27cf7a25
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-07-25 21:05:46 +0300

    feat: вынести код в отдельный компонент App

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import SearchPanel from './SearchPanel';
+import AppHeader from './AppHeader';
+import TodoList from './TodoList';
+
+const App = () => {
+  
+  const todoData = [
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 },
+  ]
+  
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList todos={ todoData } />
+    </div>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchPanel from './components/SearchPanel';
-import AppHeader from "./components/AppHeader";
-import TodoList from "./components/TodoList";
-
-const App = () => {
-  
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 },
-  ]
-  
-  return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList todos={ todoData } />
-    </div>
-  )
-}
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
