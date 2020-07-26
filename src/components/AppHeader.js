2add87eca699c4c236981257dc7c34e5c0834e28
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-07-26 18:10:16 +0300

    feat: сделать пропсы для инфо

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './../blocks/app-header/app-header.css';
 
-const AppHeader = () => {
+const AppHeader = ({ toDo, done }) => {
   return (
     <div className="app-header">
       <h1 className="app-header__title">Todo List</h1>
-      <span className="app-header__info">1 more to do, 3 done</span>
+      <span className="app-header__info">
+        {toDo} more to do, {done} done
+      </span>
     </div>
   );
 };
