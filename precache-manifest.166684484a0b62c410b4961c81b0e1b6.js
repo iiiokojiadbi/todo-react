dd333a9e9a7a353df8de5350f384374bd8316c82
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-07-29 19:32:34 +0300

    Updates

---
+++ precache-manifest.166684484a0b62c410b4961c81b0e1b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337036e6480a900711a49eac28bb8a4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbf26afbd472daef2a07",
+    "url": "/static/css/main.d9c13205.chunk.css"
+  },
+  {
+    "revision": "f5b194bf582ec3e2576a",
+    "url": "/static/js/2.098d36ea.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.098d36ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf26afbd472daef2a07",
+    "url": "/static/js/main.968c3267.chunk.js"
+  },
+  {
+    "revision": "fe39c6edd69c85b8ea65",
+    "url": "/static/js/runtime-main.5fa3d7bf.js"
+  }
+]);
